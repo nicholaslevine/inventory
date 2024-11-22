54b186e752ba729dfb16995aa3cb33a6a41be6c4
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-11-21 19:25:44 -0500

    validation

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,10 +1,38 @@
 const queries = require('../db/queries');
+const {body, validationResult} = require('express-validator');
 
-const itemValidator = [
+const itemsValidation = [
     body("name")
+        .trim()
+        .notEmpty()
+        .withMessage("Name cannot be empty")
+        .isAlpha()
+        .withMessage("Name must only contain alphabet letters"),
+    body("weight")
+        .isInt({min: 0, max: 1000})
+        .withMessage("Must be an integer between 1 and 1000"),
+    body("picture")
+        .notEmpty()
+        .withMessage("Must not be empty"),
+    body("rating")
+        .isNumeric({min:0, max:5})
+        .withMessage("Must be a number between 0 and 5"),
+    body("category")
+        .trim()
+        .notEmpty()
+        .withMessage("Category cannot be empty")
+        .isAlpha()
+        .withMessage("Category must only contain alphabet letters")
+];
+
+const categoryValidation = [
+    body("category")
+        .trim()
+        .notEmpty()
+        .withMessage("Category cannot be empty")
+        .isAlpha()
+        .withMessage("Category must only contain alphabet letters")
 ]
-
-
 
 const mainController = {
     getItems: async (req, res) => {
@@ -19,25 +47,37 @@ const mainController = {
             categories: categories
         })
     },
-    createItemPost: async (req, res) => {
-        try{
-            await queries.createRacket(req.body);
-            res.redirect('/');
-        }
-        catch(err){
-            console.error(err);
-        }
-    },
-    createCategoryPost: async (req, res) => {
-        try{
-            const {name} = req.body
-            await queries.createCategory(name);
-            res.redirect('/');
-        }
-        catch(err){
-            console.err(err)
-        }
-    },
+    createItemPost: [
+        itemsValidation,
+        async (req, res) => {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                res.send("Errors");
+            }
+            try{
+                await queries.createRacket(req.body);
+                res.redirect('/');
+            }
+            catch(err){
+                console.error(err);
+            }
+    }],
+    createCategoryPost: [
+        categoryValidation,
+        async (req, res) => {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                res.send("Errors");
+            }
+            try{
+                const {name} = req.body
+                await queries.createCategory(name);
+                res.redirect('/');
+            }
+            catch(err){
+                console.err(err)
+            }
+    }],
     createItemGet: (req, res) => {
         res.render("create-item");
     },
@@ -58,13 +98,24 @@ const mainController = {
             id: id,
         });
     },
-    updateCategoryPost: async (req, res) => {
+    updateCategoryPost: [
+        categoryValidation,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.send("Errors");
+        }
         const {category} = req.params;
         const {name} = req.body;
         await queries.updateCategory(category, name);
         res.redirect('/');
-    },
-    updateItemPost: async (req, res) => {
+    }],
+    updateItemPost: [
+        itemsValidation,
+        async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.send("Errors");}
         const {name, weight, picture, rating, category} = req.body;
         const {id} = req.params;
         await queries.updateRacket(id, {
@@ -75,7 +126,7 @@ const mainController = {
             category 
         });
         res.redirect('/');
-    },
+    }],
     deleteItem: async (req, res) => {
         const {id} = req.params;
         await queries.deleteRacket(id);    
