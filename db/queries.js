b10945e9b0adfca4c32c0f1447daa66982495983
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-11-21 20:05:50 -0500

    more error handling

--- db/queries.js
+++ db/queries.js
@@ -1,38 +1,89 @@
 const pool = require('./pool');
 
 async function getRackets(){
-    const {rows} = await pool.query('SELECT * FROM rackets WHERE name IS NOT NULL;');
-    return rows;
+    try{
+        const {rows} = await pool.query('SELECT * FROM rackets WHERE name IS NOT NULL;');
+        return rows;
+    }
+    catch(e){
+        console.error(e);
+    }
 }
 async function createRacket(racket){
-    await pool.query(`INSERT INTO rackets (name, weight, picture, rating, category) VALUES ($1, $2, $3, $4, $5)`, [racket.name, racket.weight, racket.picture, racket.rating, racket.category]);
+    try{
+        await pool.query(`INSERT INTO rackets (name, weight, picture, rating, category) VALUES ($1, $2, $3, $4, $5)`, [racket.name, racket.weight, racket.picture, racket.rating, racket.category]);
+    }
+    catch(e){
+        console.error(e);
+    }
+    
 }
 async function getRacket(name){
-    const {rows} = await pool.query(`SELECT * FROM rackets WHERE name = $1`, [name]);
-    return rows;
+    try{
+        const {rows} = await pool.query(`SELECT * FROM rackets WHERE name = $1`, [name]);
+        return rows;
+    }
+    catch(e){
+        console.error(e);
+    }
+    
 }
 async function getCategories(){
-    const {rows} = await pool.query('SELECT DISTINCT category FROM rackets');
-    return rows;
+    try{
+        const {rows} = await pool.query('SELECT DISTINCT category FROM rackets');
+        return rows;
+    }
+    catch(e){
+        console.error(e);
+    }  
 }
 async function createCategory(category){
-    await pool.query('INSERT INTO rackets (category) VALUES ($1)', [
-        category
-    ]);
+    try{
+        await pool.query('INSERT INTO rackets (category) VALUES ($1)', [
+            category
+        ]);
+    }
+    catch(e){
+        console.error(e)
+    }
+    
 }
 async function updateRacket(id, newRacket){
-    await pool.query('UPDATE rackets SET name = $1, weight = $2, picture = $3, rating = $4, category = $5 WHERE id = $6', [
-        newRacket.name, newRacket.weight, newRacket.picture, newRacket.rating, newRacket.category, id
-    ])
+    try{
+        await pool.query('UPDATE rackets SET name = $1, weight = $2, picture = $3, rating = $4, category = $5 WHERE id = $6', [
+            newRacket.name, newRacket.weight, newRacket.picture, newRacket.rating, newRacket.category, id
+        ])
+    }
+    catch(e){
+        console.error(e);
+    }
 }
 async function updateCategory(oldCategory, newCategory){
-    await pool.query('UPDATE rackets SET category = $1 WHERE category = $2', [newCategory, oldCategory]);
+    try{
+        await pool.query('UPDATE rackets SET category = $1 WHERE category = $2', [newCategory, oldCategory]);
+    }
+    catch(e){
+        console.error(e);
+    }
+   
 }
 async function deleteRacket(id){
-    await pool.query('UPDATE rackets SET name = NULL, weight = NULL, picture = NULL, rating = NULL WHERE id = $1', [id])
+    try{
+        await pool.query('UPDATE rackets SET name = NULL, weight = NULL, picture = NULL, rating = NULL WHERE id = $1', [id])
+    }
+    catch(e){
+        console.error(e);
+    }
+    
 }
 async function deleteCategory(category){
-    await pool.query(`DELETE FROM rackets WHERE category = $1`, [category])
+    try{
+        await pool.query(`DELETE FROM rackets WHERE category = $1`, [category])
+    }
+    catch(e){
+        console.error(e);
+    }
+    
 };
 
 
